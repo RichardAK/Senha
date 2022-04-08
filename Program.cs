c04bbe88541fa394ed73935749a08711f4fde97e
Kauã <Kau.lira@yahoo.com>
2022-04-08 17:48:48 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿const string senhacorreta = "MbnaMedia";
+string senha,acessopermitido;
+
+Console.Write("Digiote sua senha:"); 
+senha = Console.ReadLine()!; 
+
+acessopermitido = senha == senhacorreta? "permitido" : "Negado";
+
+Console.WriteLine($"Acesso {acessopermitido}"); 
+
+
